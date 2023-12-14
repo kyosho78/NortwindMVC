352fa738bc0446a2122baeb95daaee14c462460b
Valter Backström <valter.backstrom@student.careeria.fi>
2023-12-14 15:34:21 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using NortwindMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NortwindMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NorthwindOriginalEntities db = new NorthwindOriginalEntities();
+
+
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                ViewBag.LoggedStatus = "Out";
+            }
+            else ViewBag.LoggedStatus = "In";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Yhtiön perustietojen kuvailua";   //Tämä muuttaa About.cshtml tekstiä
+            ViewBag.Info = "Ole tarkkana koodatessa!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Saapumisohje";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.UserName;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivulle
+        }
+    }
+}
---
+++ Controllers/ShippersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NortwindMVC.Models;
+
+namespace NortwindMVC.Controllers
+{
+    public class ShippersController : Controller
+    {
+        private NorthwindOriginalEntities db = new NorthwindOriginalEntities();
+
+        // GET: Shippers
+        public ActionResult Index()
+        {
+            var shippers = db.Shippers.Include(s => s.Region);
+            return View(shippers.ToList());
+        }
+
+        // GET: Shippers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shippers);
+        }
+
+        // GET: Shippers/Create
+        public ActionResult Create()
+        {
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription");
+            return View();
+        }
+
+        // POST: Shippers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShipperID,RegionID,CompanyName,Phone")] Shippers shippers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shippers.Add(shippers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);
+        }
+
+        // GET: Shippers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);
+        }
+
+        // POST: Shippers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShipperID,RegionID,CompanyName,Phone")] Shippers shippers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shippers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);
+        }
+
+        // GET: Shippers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shippers);
+        }
+
+        // POST: Shippers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shippers shippers = db.Shippers.Find(id);
+            db.Shippers.Remove(shippers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
